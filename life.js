d772b7076d2a75118f3ad332f8752ba7480b4ea8
Frederick Polgardy <fred@polgardy.com>
2011-03-09 11:15:36 -0600

    patch browsers that don't have Function#bind

--- life.js
+++ life.js
@@ -1,3 +1,15 @@
+if (!Function.prototype.bind) { // ECMAScript 5
+  Function.prototype.bind = function(context) {
+    var self  = this
+      , slice = [].slice
+      , args  = slice.call(arguments, 1)
+
+    return function() {
+      return self.apply(context, args.concat(slice.call(arguments)))
+    }
+  }
+}
+
 function Set() {
   this.storage = {}
 }
@@ -59,8 +71,7 @@ var neighboringOffsets = [
 ]
 
 Point.prototype = {
-  // for set key
-  toString: function() {
+  toString: function() { // for set key
     return this.x + ':' + this.y
   },
 
